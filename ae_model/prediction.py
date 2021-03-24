deffdaed58a5f1cce0c5f0fc6ea543d362eed82d
Wong Ming Jie <wongmingjie@wongs-mbp.lan>
2021-03-25 00:56:21 +0800

    Update prediction.py

--- ae_model/prediction.py
+++ ae_model/prediction.py
@@ -11,10 +11,10 @@ torch.manual_seed(42)
 class Config(object):
     def __init__(self):
         self.BATCH_SIZE = 64
-        self.k_folds = 12
+        self.k_folds = 3 #12
         self.lr_scheduler = None
         self.test_size = .1
-        self.epoch = 200
+        self.epoch = 5 #200
 
 class Loss:
     def __init__(self):
